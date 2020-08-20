bf5e0b67ca515d30c3d983d8c32bf7329c8791c8
Chandra Nguyen <chandra.nguyen@outlook.com>
2020-08-20 15:53:27 -0600

    Completed proxy server set up!

--- proxy/server.js
+++ proxy/server.js
@@ -11,6 +11,45 @@ app.use((req, res, next) => {
 
 app.use('/:id', express.static(__dirname + '/lib'));
 
+
+app.get('/api/images/:id' , (req, res) => {
+  request(
+    { url: `http://localhost:3001/api/images/${req.params.id}` },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: error.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  )
+});
+
+app.get('/api/booking/:id' , (req, res) => {
+  request(
+    { url: `http://localhost:3002/api/booking/${req.params.id}` },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: error.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  )
+});
+
+app.get('/assets/airbnb_rating_star.png' , (req, res) => {
+  request(
+    { url: `http://localhost:3002/api/booking/${req.params.id}` },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: error.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  )
+});
 app.get('/api/overall_reviews/:id', (req, res) => {
   request(
     { url: `http://localhost:3000/api/overall_reviews/${req.params.id}` },
@@ -35,20 +74,6 @@ app.get('/api/individual_reviews/:id' , (req, res) => {
     }
   )
 });
-
-app.get('/api/images/:id' , (req, res) => {
-  request(
-    { url: `http://localhost:3001/api/images/${req.params.id}` },
-    (error, response, body) => {
-      if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: 'error', message: error.message });
-      }
-
-      res.json(JSON.parse(body));
-    }
-  )
-});
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
